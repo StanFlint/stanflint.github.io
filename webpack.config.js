391b21d9d8231903f224bd64d3ddf76e29bc9b61
StanFlint <stanleyjflint@gmail.com>
2022-09-10 20:17:50 +0100

    :wrench: Created webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+}
