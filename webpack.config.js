d766c166080027811cf15d9b3416c8203523d983
StanFlint <stanleyjflint@gmail.com>
2022-09-13 11:49:48 +0100

    :pencil2: Get current webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/[name].[contenthash][ext][query]',
-    clean: true
-  },
-  plugins: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode !== 'development'
+
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: './src/template.html'
