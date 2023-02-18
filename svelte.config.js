c65b432a15bbd50c57eb385d87b49bf7185ab235
StanFlint <stanleyjflint@gmail.com>
2023-02-18 16:25:05 +0000

    :wrench: Added prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
  
 export default {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       pages: 'build',
