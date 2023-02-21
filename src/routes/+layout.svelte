e9f14e48d47e8271e2b558c14ecafb544f388a2d
StanFlint <stanleyjflint@gmail.com>
2023-02-21 13:30:24 +0000

    :lipstick: +layout define font and global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,20 @@
-<slot></slot>
+<slot />
+
+<style lang="scss">
+	@font-face {
+		font-family: Poppins;
+		src: url('$asset/font/Poppins-Regular.ttf');
+	}
+
+	@font-face {
+		font-family: Poppins;
+		font-weight: 700;
+		src: url('$asset/font/Poppins-Bold.ttf');
+	}
+
+	:global(html, body) {
+		margin: 0;
+		height: 100%;
+		font-family: Poppins;
+	}
+</style>
