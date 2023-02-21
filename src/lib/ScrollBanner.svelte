c8377b41c0fb79271af4d1b5f3b85227841d1581
StanFlint <stanleyjflint@gmail.com>
2023-02-21 11:30:40 +0000

    :pencil2: ScrollBanner title props

--- src/lib/ScrollBanner.svelte
+++ src/lib/ScrollBanner.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+	export let title = '';
 </script>
 
-<div />
+<div class="banner-background">
+	<div class="banner-center">
+		<div class="banner-container">
+			<h1 class="banner-scroll">
+				{title}
+			</h1>
+		</div>
+	</div>
+</div>
 
 <style lang="scss">
 </style>
