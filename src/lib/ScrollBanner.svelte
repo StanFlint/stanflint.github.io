4b5a39b16b39ef68202d8b3e6ffa33af4fbc5258
StanFlint <stanleyjflint@gmail.com>
2023-02-21 11:35:23 +0000

    :lipstick: ScrollBanner added styles

--- src/lib/ScrollBanner.svelte
+++ src/lib/ScrollBanner.svelte
@@ -13,4 +13,43 @@
 </div>
 
 <style lang="scss">
+	.banner-background {
+		background-image: linear-gradient(to right, #ff00e5, #00c2ff);
+	}
+
+	.banner-center {
+		margin: auto;
+		max-width: 900px;
+		width: 100%;
+	}
+
+	.banner-container {
+		margin: 0 1.5rem;
+		overflow: hidden;
+	}
+
+	.banner-scroll {
+		@extend %banner-scroll-style;
+		animation-name: banner-scroll;
+		margin: 0px;
+	}
+
+	%banner-scroll-style {
+		font-size: 2.5rem;
+		font-weight: bold;
+		font-style: italic;
+		color: white;
+		white-space: nowrap;
+		animation-timing-function: linear;
+		animation-iteration-count: infinite;
+	}
+
+	@keyframes banner-scroll {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(-100%);
+		}
+	}
 </style>
