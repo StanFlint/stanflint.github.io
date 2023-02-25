73bccd307d293f36a538a49532ed3b54bc3b9476
StanFlint <stanleyjflint@gmail.com>
2023-02-25 22:10:02 +0000

    :sparkles: Created ArticleGallery

---
+++ src/lib/article/ArticleGallery.svelte
@@ -0,0 +1,15 @@
+<div class="gallery">
+	<slot />
+</div>
+
+<style lang="scss">
+	.gallery {
+		display: grid;
+		grid-template-columns: auto;
+		gap: 1.5rem;
+
+		@media screen and (max-width: 900px) {
+			grid-template-columns: auto;
+		}
+	}
+</style>
